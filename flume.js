69fe45156170544a7bb244754262ec03f13ebd7f
anonfoxer <kevinandlife@gmail.com>
2020-12-21 21:37:14 -0600

    Rewrote & cleaned up entry point.
    Update 2.0! Check the entry (flume.js) for details!

--- flume.js
+++ flume.js
@@ -2,11 +2,7 @@ const Discord = require('discord.js'); //create client
 const commando = require('discord.js-commando');
 const {TOKEN} = require('./secret');
 const {OWNER} = require('./secret2');
-var fs = require('fs');
 
-//const bot = new Discord.Client();
-//let cooldown = new.Set();
-//let cdseconds = 20;
 const bot = new commando.Client({
     commandPrefix: '&',
     
@@ -15,13 +11,20 @@ const bot = new commando.Client({
     disableEveryone: true
 });
 
+/* Flume 2.0 - What's new? 
+- Rewrite of some of the code base.
+- Cleaned up entry (flume.js) 
+- Added kick & ban commands
+- Removed mute & unmute
+- Removed setup commands.
+- Added info command.
+*/
+
 bot.on('ready', ( ) => {
     bot.user.setActivity('&help')
-    console.log("FlumeBot Online!")
-    //message.channel.send('Raidmode is OFF by default.')
+    console.log("FlumeBot Online!");
+    console.log("Use &help to get started!");
 })
-
-let raidmode = 0;
 
 bot.on('guildMemberAdd', member => {
     console.log("User " + member.user.username + " Joined!"); //announce new member to the server.
@@ -30,24 +33,8 @@ bot.on('guildMemberAdd', member => {
 
 /* allocate commands */
 registerCommands();
-logit();
-/* bot.on('message', (message) =>{ //Raidmode Commands
-
-
-    if(message.content == '~raidmode ON'){ 
-        if(raidmode = on) return;
-        if(raidmode = off) {
-            let raidmode = on;
-            if(!message.member)
-        }
-
-    }
-
-}); */
-
 
 function registerCommands() { //registers all commands. Not neccissarily reserved as a func but useful to name it this
-    bot.registry.registerGroup('setup', 'Setup'); //Sets command group Random
     bot.registry.registerGroup('fun', 'Fun');
     bot.registry.registerGroup('useful', 'Useful');
     bot.registry.registerGroup('moderation', 'Moderation');
@@ -55,20 +42,5 @@ function registerCommands() { //registers all commands. Not neccissarily reserve
     bot.registry.registerCommandsIn(__dirname + "/commands"); //commands found in <dicrec>\commands.
 }
 
-//THE BELOW FUNCTION IS A *GLOBAL LOG!* IF YOU DOWNLOAD THIS LOG YOU ARE LOOKING AT THE BOTS RUN HISTORY FOR ITS UPTIME, ACROSS ALL SERVERS IT IS IN!
 
-function logit(message) {
-    var now = new Date();
-    var message = "FlumeBot Online! Started on: " + Date.now() + "\n";
-    console.log(message);
-    fs.writeFileSync('./devinfo/log.txt', message, err=> {
-        if (err) {
-            console.error(err)
-            return
-        }
-    })
-}
-
-
-
-bot.login(TOKEN); //login bot 
+bot.login(TOKEN);

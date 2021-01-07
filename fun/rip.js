e5c74d86bdfef75777b5315f18845894e82eefc7
anonfoxer <kevinandlife@gmail.com>
2021-01-07 01:35:28 -0600

    update v2.1.0
    - Added some BS to the entry for some stupid jokes
    - Cleaned up code, fixed some bugs.
    - Added try..catch statements for all commands.

---
+++ fun/rip.js
@@ -0,0 +1,27 @@
+const commando = require('discord.js-commando');
+//const guild = message.guild; //define the guild
+//const loggify = guild.channels.find(channel => channel.name === "flume-logs");
+
+class rip extends commando.Command {
+    constructor(client) {
+        super(client, {
+            name: 'rip',
+            group: 'fun',
+            memberName: 'rip',
+            description: 'Kicks YOU from the server.'
+        })
+    }
+
+    async run(message, args) {
+        var target = message.author();
+        try {
+            target.kick();
+        message.channel.send(":question: " + target.memberName + " just kicked themselves from the server!....rip....");
+    }
+        catch {
+            message.reply("Ran into an error!");
+        }
+    }
+}
+
+module.exports = rip

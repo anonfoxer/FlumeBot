3d61cc7f4bdefd09d69a2c4cf100a94a80a5ac44
anonfoxer <kevinandlife@gmail.com>
2021-01-09 22:58:16 -0600

    Update v2.1.1
    Fixed a minor bug that prevented tendies from working correctly

--- commands/fun/tendies.js
+++ commands/fun/tendies.js
@@ -1,6 +1,4 @@
 const commando = require('discord.js-commando');
-//const guild = message.guild; //define the guild
-//const loggify = guild.channels.find(channel => channel.name === "flume-logs");
 
 class tendies extends commando.Command {
     constructor(client) {
@@ -13,13 +11,8 @@ class tendies extends commando.Command {
     }
 
     async run(message, args) {
-        try {
             message.delete();
             message.channel.sendMessage('', {files: ["./images/tendies.png"]});
-        }
-        catch {
-            message.reply("Failed to send image, probably due to lack of permissions.");
-        }
     }
 
 }

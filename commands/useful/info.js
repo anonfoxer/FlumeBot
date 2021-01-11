5c56c0b5877d38c5c828e9f2e8d4786d3dcdad38
anonfoxer <kevinandlife@gmail.com>
2021-01-11 11:08:59 -0600

    Small tweaks

--- commands/useful/info.js
+++ commands/useful/info.js
@@ -1,6 +1,5 @@
 const commando = require('discord.js-commando');
 
-
 class inform extends commando.Command {
     constructor(client) {
         super(client, {
@@ -13,11 +12,11 @@ class inform extends commando.Command {
 
     async run(message, args) {
         try {
-            message.channel.send(":wave: Hi there! Im Flume, developed by anonfoxer#8098!");
+            message.channel.send(":wave: Hi there! Im Flume, developed by anonfoxer! https://github.com/anonfoxer/FlumeBot");
             message.channel.send(":wrench: Please make sure I have the following permissions:\n\n:red_circle: -Admin Privs\n:red_circle: -Manage Roles\n:red_circle: -Kick & Ban Members\n:red_circle: -Manage Messages\n:red_circle: -Create Channels");
         }
         catch {
-            message.reply("An error occured, probably due to network issues. Contact anonfoxer#8098.");
+            message.reply("An error occured, probably due to network issues. Contact anonfoxer by issuing a pull request or bug report.");
         }
     }
 }

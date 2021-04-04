3bbff1855793af14b10b34c7e9c0c20f8c6ff45d
Magic <kevinandlife@gmail.com>
2021-04-04 17:26:24 -0500

    2.4.1 | Correct spelling lol

--- commands/aminals/tortise.js
+++ commands/aminals/tortise.js
@@ -1,27 +1,27 @@
 const commando = require('discord.js-commando');
 const Discord = require('discord.js'); //create client
 const pixabay = require("pixabay-api");
-const pixakey = null;
+const pixakey = "20172613-dcbb496c2037665ce6f5a46b1";
 
 class tortIs extends commando.Command {
     constructor(client) {
         super(client, {
-            name: 'tortise',
+            name: 'tortoise',
             group: 'aminals',
-            memberName: 'tortise',
-            description: 'Gets a pic of a tortise'
+            memberName: 'tortoise',
+            description: 'Gets a pic of a tortoise'
         })
     }
 
     async run(message, {title}) {
         try {
-            var r = await pixabay.searchImages(pixakey, 'tortise')
+            var r = await pixabay.searchImages(pixakey, 'tortoise')
             message.reply(new Discord.MessageEmbed()
-                .setTitle("Random tortise")
+                .setTitle("Random tortoise")
                 .setImage(r.hits[Math.floor(Math.random() * r.hits.length)].largeImageURL))
         }
         catch(err) {
-            message.reply("An error occured: " + err);
+            message.reply("An error occured, contact anonfoxer#8098. Error details: " + err);
         }
     }
 }

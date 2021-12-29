09522e83608d1f1b74620d2346a626407076b306
Magic <kevinandlife@gmail.com>
2021-12-29 17:45:32 -0600

    Update chaneglog.js

--- commands/useful/chaneglog.js
+++ commands/useful/chaneglog.js
@@ -18,10 +18,10 @@ class changeLog extends commando.Command {
             .setTitle('Changelog')
             .setURL('https://github.com/anonfoxer/FlumeBot')
             .setAuthor('FlumeBot', 'https://i.imgur.com/agTGFwL.png', 'https://github.com/anonfoxer/FlumeBot')
-            .setDescription('Changelog for v2.5.1')
+            .setDescription('Changelog for v2.5.1.2')
             .setThumbnail('https://i.imgur.com/VH3jv7d.jpg')
             .addFields(
-                { name: 'Changes in v2.5.1', value: 'Removed tenacious and friendly from &badbuild\nStarted doing fancy new embeds for most important messages.\nAdded &haustrat\nRemoved broken message.author() calls from functions that used them to prevent errors with the new discord.js update.' },
+                { name: 'Changes in v2.5.1', value: 'Changed up the &info command for 3.0 (2.5.1.1)\nFixed a rare crash when a user joins a server.' },
             )
 
 

8f468e36352c52299ec07ea50e3df8c9e7cd1e30
Magic <kevinandlife@gmail.com>
2021-12-30 16:32:02 -0600

    Update chaneglog.js

--- commands/useful/chaneglog.js
+++ commands/useful/chaneglog.js
@@ -18,10 +18,10 @@ class changeLog extends commando.Command {
             .setTitle('Changelog')
             .setURL('https://github.com/anonfoxer/FlumeBot')
             .setAuthor('FlumeBot', 'https://i.imgur.com/agTGFwL.png', 'https://github.com/anonfoxer/FlumeBot')
-            .setDescription('Changelog for v2.5.1.2')
+            .setDescription('Changelog for v2.5.2')
             .setThumbnail('https://i.imgur.com/VH3jv7d.jpg')
             .addFields(
-                { name: 'Changes in v2.5.1.2', value: 'Changed up the &info command for 3.0 (2.5.1.1)\nFixed a rare crash when a user joins a server.' },
+                { name: 'Changes in v2.5.2', value: 'Changed descriptions for Mordhau command embeds.\nAdded the ability for the bot to tease you for editing a message. (Thank you DriftingNitro)' },
             )
 
 
